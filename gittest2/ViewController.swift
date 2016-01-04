cda4ae939ead76cc5936fb9a535783320487a505
안수근 <ansugeun@ansugeunui-imac.local>
2016-01-04 23:25:28 +0900

    Initial Commit

---
+++ gittest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest2
+//
+//  Created by 안수근 on 2016. 1. 4..
+//  Copyright © 2016년 안수근. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
